9687645b4b5b312155502041ba3edfae3658e10c
shahedshosha <102873895+shahedshosha@users.noreply.github.com>
2022-05-14 15:22:02 +0300

    creat

--- 2_tier_app/Users.cs
+++ 2_tier_app/Users.cs
@@ -18,7 +18,36 @@ namespace _2_tier_app
         public frmUsers()
         {
             InitializeComponent();
-            connection = new SqlConnection(connectionString);
+           connection = new SqlConnection(connectionString);
+        }
+        private void Form1_load(object sender, EventArgs e)
+        {
+            string command = "slect * from Users";
+            SqlCommand sqlcommand  = new SqlCommand(command, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlcommand);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+        private void bntsave_click(object sender, EventArgs e)
+        {
+            string userFullname = txtFullName.Text;
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            string command = $"Insert into Users values('{userFullname}',  '{username}', {password} ";
+            SqlCommand sqlcommand = new SqlCommand(command , connection);
+
+           
+            if(connection.State != ConnectionState.Open )
+            connection.Open();
+            MessageBox.Show(connection.State.ToString());
+            sqlCommand.ExecuteNonQuery();
+            connection.Close();
+
+           }
+
         }
     }
 }
